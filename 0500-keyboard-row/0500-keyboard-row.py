22cd5667c6a8cbba29c05d6c661fbfa73f2c2305
kooshant kumar <143017244+kooshant@users.noreply.github.com>
2026-09-24 23:13:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (25.69%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        s1="qwertyuiopQWERTYUIOP"
+        s2="asdfghjklASDFGHJKL"
+        s3="zxcvbnmZXCVBNM"
+        cnt1,cnt2,cnt3=0,0,0
+        valid=True
+        res=[]
+        for word in words:
+            cnt1,cnt2,cnt3=0,0,0
+            valid=True
+            for i in range(len(word)):
+                if word[i] in s1:
+                    cnt1+=1
+                elif word[i] in s2:
+                    cnt2+=1
+                else:
+                    cnt3+=1
+                if cnt1!=i+1 and cnt2!=i+1 and cnt3!=i+1:
+                    valid=False
+                    break            
+            if(valid):
+                res.append(word)
+        return res
