15875598d4fa4efcc60fe4df79f33b9578063541
kooshant kumar <143017244+kooshant@users.noreply.github.com>
2026-09-26 12:01:00 +0530

    Time: 0 ms (100%), Space: 19.3 MB (25.6%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,13 @@
+
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        prev1, prev2 = 0, 0
+        for num in nums:
+            prev1, prev2 = max(prev2 + num, prev1), prev1
+
+        return prev1
